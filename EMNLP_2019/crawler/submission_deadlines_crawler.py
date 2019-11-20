c9fa15a626f400e63f4dfa3749c2eb2790eef9e1
yuqing <yuqingxu0506@163.com>
2019-11-20 22:52:37 +0800

    finished crawler for submission deadlines

---
+++ EMNLP_2019/crawler/submission_deadlines_crawler.py
@@ -0,0 +1,34 @@
+"""Crawler to collect the EMNLP 2019 submission deadlines/important dates """
+__author__ = "Yuqing_Xu"
+
+
+import requests
+import bs4
+from bs4 import BeautifulSoup
+import json
+
+def submission_deadlines_crawler():
+    """
+       Extracts all information available for submission important dates at
+       https://www.emnlp-ijcnlp2019.org/calls/papers .
+       :return: a list of a dictionaries with a deadline represented as one dictionary.
+       """
+    page = requests.get("https://www.emnlp-ijcnlp2019.org/calls/papers")
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    submission_deadlines = []
+    tables = soup.find_all('table')
+    tab = tables[0].get_text()
+    array1 = [i for i in tab.split('\n') if i != '']
+    array2 = []
+    [array2.append(array1[i:i+3]) for i in range(0,len(array1),3)]
+    for i in array2:
+        submission_deadline = {attribute: None for attribute in ["name", "date"]}
+        submission_deadline['name'] = i[0]
+        submission_deadline['date'] = i[2]
+        submission_deadlines.append(submission_deadline)
+    #print(submission_deadlines)
+    #print(json.dumps(submission_deadlines, indent=1))
+    return(submission_deadlines)
+
+
