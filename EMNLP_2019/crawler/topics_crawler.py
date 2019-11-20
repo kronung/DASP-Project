bd63a05e9f5f174d04779bfc41748ecdec1b5033
Lars Meister <meista95@googlemail.com>
2019-11-20 19:07:23 +0100

    small changes in crawlers, combine crawler results

--- EMNLP_2019/crawler/topics_crawler.py
+++ EMNLP_2019/crawler/topics_crawler.py
@@ -6,7 +6,7 @@ import bs4
 from bs4 import BeautifulSoup
 import json
 
-def topics_crawler():
+def extract_topics():
     """
     Extracts all information available for topics at
     https://www.emnlp-ijcnlp2019.org/calls/papers .
@@ -15,12 +15,9 @@ def topics_crawler():
     page = requests.get("https://www.emnlp-ijcnlp2019.org/calls/papers")
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    topic_list = []
     strings = soup.select("section > ul")[0].get_text()
-    array = strings.split('\n')
-    array = [i for i in array if i != '']
-    dic = {'topics':array}
-    topic_list.append(dic)
-    #print(topic_list)
+    topics = strings.split('\n')
+    topics = [i for i in topics if i != '']
     #print(json.dumps(topic_list, indent=1))
-    return topic_list
+    return topics
+
--- EMNLP_2019/emnlp_crawler.py
+++ EMNLP_2019/emnlp_crawler.py
@@ -3,18 +3,21 @@ __author__= "Lars Meister"
 
 import json
 
-from EMNLP_2019.crawler import tutorial_crawler, workshop_crawler, keynote_crawler, paper_crawler
+from EMNLP_2019.crawler import tutorial_crawler, workshop_crawler, keynote_crawler, paper_crawler, \
+    topics_crawler, organizers_crawler, submission_deadlines_crawler
 
 
 def collect_data():
     conf_dict = generate_conference_dummy_dict()
     #print(json.dumps(conf_dict, indent=1))
+    conf_dict["topics"] = topics_crawler.extract_topics()
+    conf_dict["organizers"] = organizers_crawler.extract_organizers()
+    conf_dict["submission_deadlines"] = submission_deadlines_crawler.extract_submission_deadlines()
     conf_dict["name"] = "EMNLP 2019"
     conf_dict["tutorials"] = tutorial_crawler.extract_tutorials()
     conf_dict["workshops"] = workshop_crawler.extract_workshops()
     conf_dict["keynotes"] = keynote_crawler.extract_keynotes()
     conf_dict["papers"] = paper_crawler.extract_papers()
-    # TODO need general information
 
     with open("output/emnlp2019_data.json", "w", encoding='utf-8') as f:
         json.dump({"EMNLP2019": conf_dict}, f, ensure_ascii=False)
