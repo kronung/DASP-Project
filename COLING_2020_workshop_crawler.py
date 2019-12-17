6c4f8f471853f289d94405bad84a36501aa282f9
yuqing <yuqingxu0506@163.com>
2019-12-17 20:04:24 +0100

    crawler for COLING2020 workshops

---
+++ COLING_2020_workshop_crawler.py
@@ -0,0 +1,37 @@
+"""Crawler to collect the COLING 2020 workshops"""
+__author__ = "Yuqing Xu"
+
+from bs4 import BeautifulSoup
+from urllib import request
+import json
+
+
+def extract_workshops():
+    """
+    Extracts all information available for workshops provided at
+    https://coling2020.org/pages/workshops
+    :return: list of dictionaries with a workshop represented as one dictionary.
+    """
+    workshops = []
+    url = "https://coling2020.org/pages/workshops"
+
+    try:
+        page = request.urlopen(url)
+    except:
+        print("Could not connect to url.")
+
+    soup = BeautifulSoup(page, 'html.parser').find("section", {"id": "main_content"})
+
+    for child in soup.findChildren('h3'):
+        for i in child.findNext('ul').find_all('li'):
+            workshop = {attribute: None for attribute in
+                        ['title', 'authors', 'abstract', 'datetime', 'location', 'link']}
+            workshop['datetime'] = child.text
+            workshop['title'] = i.find('a').text
+            workshop['link'] = i.find('a')['href']
+            workshops.append(workshop)
+
+    #print(json.dumps(workshops, indent=1))
+    return workshops
+
+
